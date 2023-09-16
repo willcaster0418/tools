94e54fcf3308f64973af2cf598bbda4025219518
Sang-Hun, Kim <fury8208@gmail.com>
2023-09-16 22:30:40 +0900

    commit for key

--- hotkey_process.py
+++ hotkey_process.py
@@ -1,42 +1,56 @@
-from pynput import keyboard
-keymap_position = {'q' : ('m', 0.2, 0.2), 'w' : ('m', 0.5, 0.2), 'e' : ('m', 0.8, 0.2),
-                   'a' : ('m', 0.2, 0.5), 's' : ('m', 0.5, 0.5), 'd' : ('m', 0.8, 0.5),
-                   'z' : ('m', 0.2, 0.8), 'x' : ('m', 0.5, 0.8), 'c' : ('m', 0.8, 0.8),
-                   '1' : ('s', 1, 0), '2' : ('s', 2, 0)}
-proc_position = {}
 import win32api
 import pyautogui
 import win32gui
-def factory(key):
-    global keymap_position
-    def on_activate():
-        screen_size = pyautogui.size()
-        pos = keymap_position[key]
-        if pos[0] == 'm':
-            x, y = (pos[1] * screen_size[0], pos[2] * screen_size[1])
-            win32api.SetCursorPos((int(x), int(y)))
-            pyautogui.click(int(x), int(y))
-        elif pos[0] == 's':
-            # move forground window to monitor 2
-            handle = win32gui.GetForegroundWindow()
-            monitors = win32api.EnumDisplayMonitors()
-            if len(monitors) >= int(key):
-                win32gui.MoveWindow(handle, 
-                                    monitors[int(key) - 1][2][0],
-                                    monitors[int(key) - 1][2][1],
-                                    monitors[int(key) - 1][2][2],
-                                    monitors[int(key) - 1][2][3],
-                                    True)
-    return on_activate
+from pynput import keyboard
+class HotkeyProcess:
+    def __init__(self):
+        self.keymap_position = {'q' : ('m', 0.2, 0.2), 'w' : ('m', 0.5, 0.2), 'e' : ('m', 0.8, 0.2),
+                                'a' : ('m', 0.2, 0.5), 's' : ('m', 0.5, 0.5), 'd' : ('m', 0.8, 0.5),
+                                'z' : ('m', 0.2, 0.8), 'x' : ('m', 0.5, 0.8), 'c' : ('m', 0.8, 0.8),
+                                '1' : ('s', 1, 0), '2' : ('s', 2, 0),
+                                't' : ('v',   0, -10), 'g' : ('v',  0, 10),
+                                'f' : ('v', -10,   0), 'h' : ('v', 10,  0), 'r' : ('c', 0, 0), 'y' : ('l', 0, 0)}
+        self.proc_position = {}
+        for key in self.keymap_position.keys():
+            self.proc_position[f"<ctrl>+<alt>+{key}"] = self.factory(key)
 
-for key in keymap_position.keys():
-    proc_position[f"<ctrl>+<alt>+{key}"] = factory(key)
+    def __call__(self):
+        self.proc()
 
+    def __str__(self):
+        return f"HotkeyProcess({self.keymap},{self.proc})"
+
+    def factory(self, key):
+        def on_activate():
+            screen_size = pyautogui.size()
+            pos = self.keymap_position[key]
+            if pos[0] == 'm':
+                x, y = (pos[1] * screen_size[0], pos[2] * screen_size[1])
+                win32api.SetCursorPos((int(x), int(y)))
+                # pyautogui.click(int(x), int(y))
+            elif pos[0] == 'v':
+                x, y = win32api.GetCursorPos()
+                win32api.SetCursorPos((x + pos[1], y + pos[2]))
+            elif pos[0] == 'l':
+                x, y = win32api.GetCursorPos()
+                pyautogui.click(int(x), int(y), button='right')
+            elif pos[0] == 'c':
+                x, y = win32api.GetCursorPos()
+                pyautogui.click(int(x), int(y))
+            elif pos[0] == 's':
+                # move forground window to monitor 2
+                handle = win32gui.GetForegroundWindow()
+                monitors = win32api.EnumDisplayMonitors()
+                if len(monitors) >= int(key):
+                    win32gui.MoveWindow(handle, 
+                                        monitors[int(key) - 1][2][0],
+                                        monitors[int(key) - 1][2][1],
+                                        monitors[int(key) - 1][2][2],
+                                        monitors[int(key) - 1][2][3],
+                                        True)
+        return on_activate
+
+HotkeyInstance = HotkeyProcess()
 # print(proc_position)
-with keyboard.GlobalHotKeys(proc_position) as h:
-    while True:
-        #h.join()
-        h.run()
-        print("h.run()")
-        break
-    h.stop()
+with keyboard.GlobalHotKeys(HotkeyInstance.proc_position) as h:
+    h.join()
--- test.py
+++ test.py
@@ -1,12 +1,38 @@
-import win32api
-import pyautogui
-pos = [0.5, 0.5]
-screen_size = pyautogui.size()
-print(screen_size)
-monitors = win32api.EnumDisplayMonitors()
-print(monitors)
+import pynput
+from pynput.keyboard import Key, Controller
 
-import win32gui
-# move forground window to monitor 2
-handle = win32gui.GetForegroundWindow()
-win32gui.MoveWindow(handle, 1920, 0, 1920, 1080, True)
+# 키 입력 이벤트를 처리하는 핸들러 함수
+def on_key_input(key):
+    # 키 입력 처리 코드
+    print(key)
+
+# 서비스의 시작 함수
+def on_start():
+    # 키 입력 이벤트 핸들러 등록
+    keyboard = Controller()
+    keyboard.on_press(on_key_input)
+
+# 서비스의 이벤트 처리 함수
+def on_event(event):
+    # 키 입력 이벤트 처리
+    print("키 입력 이벤트 수신")
+
+if __name__ == "__main__":
+    # 서비스 등록
+    from win32serviceutil import RegisterServiceCtrlHandlerEx
+    from win32service import Service
+
+    class MyService(Service):
+        def __init__(self, name, description):
+            super().__init__(name, description)
+
+        def on_start(self):
+            on_start()
+
+        def on_stop(self):
+            pass
+
+    service_name = "pynput_service"
+    service_description = "pynput service"
+    RegisterServiceCtrlHandlerEx(service_name, on_event, None)
+    MyService(service_name, service_description).start()
